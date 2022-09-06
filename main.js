8c8d3db4da997ce0d1b7abd767e6c956aa4f40c7
abaduna <artuelrey@gmail.com>
2022-09-06 02:12:08 -0230

    app web responsive

---
+++ main.js
@@ -0,0 +1,15 @@
+document.querySelector(".menu-btn-mini").addEventListener("click",()=>{
+   document.querySelector(".nav-menu").classList.toggle("show");
+})
+
+ScrollReveal().reveal('.menu-btn-mini',{deley:500});
+ScrollReveal().reveal('.nav-main',{deley:500});
+ScrollReveal().reveal('.show-case',{deley:500});
+ScrollReveal().reveal('.new_cards',{deley:500});
+ScrollReveal().reveal('.cards-baner-two',{deley:500});
+ScrollReveal().reveal('.cards-baner-two',{deley:500});
+ScrollReveal().reveal('.socialo',{deley:500});
+ScrollReveal().reveal('.cards-baner-two',{deley:500});
+ScrollReveal().reveal('.foder-link',{deley:500});
+ScrollReveal().reveal('.footer',{deley:500});
+
